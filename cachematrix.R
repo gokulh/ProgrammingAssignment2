35644e99bde13c230b286856d8e08ec1e6bc0adc
gokulh <31223005+gokulh@users.noreply.github.com>
2017-08-21 22:15:16 -0400

    Update cachematrix.R
    > m <- matrix(5:8,2,2)
    > m
         [,1] [,2]
    [1,]    5    7
    [2,]    6    8
    > mcm <- makeCacheMatrix(m)
    > cacheSolve(mcm)
         [,1] [,2]
    [1,]   -4  3.5
    [2,]    3 -2.5
    >

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,55 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  i <- NULL
 
+    #set the matrix
+    set <- function(y) {
+      x <<- y
+      i <<- NULL
+    }
+    
+    #get the matrix
+    get <- function() { 
+      x 
+    }
+    
+    #sets the inverse of a matrix
+    setinverse <- function(inv) { 
+      i <<- inv 
+    }
+    
+    #return the inverse of a matrix
+    getinverse <- function() { 
+      i 
+    }
+  
+    #returns a list of functions
+  list(set = set, get = get, getinverse = getinverse, setinverse =  setinverse)
+  
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv = x$getinv() #if the inv is already calculate get it.
+  
+  #Check if the inv value from above line is not NULL. then return the value from above line.
+  if (!is.null(inv)){
+    # get it from the cache and skips the computation. 
+    message("getting cached data")
+    return(inv)
+  }
+  
+  #Calculate the inverse using the solve fuction.
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  #set the value of inv in cache. So it does not have to calculate again.
+  x$setinv(inv)
+  
+  #return the inv matrix
+  return(inv)
 }
